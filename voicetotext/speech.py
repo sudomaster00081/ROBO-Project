71d7d74b5ad0a99b553cc3f00e0eb4fd0c8359ce
Aj <a.jayprasadd0008@gmail.com>
2023-06-28 11:57:21 +0530

    Revert "bump"
    This reverts commit baf9afede4ccfb43c035dd49efcf8d51e21b289b.

--- voicetotext/speech.py
+++ voicetotext/speech.py
@@ -1,28 +1,29 @@
-import pocketsphinx
+import speech_recognition as sr
 
 def voice_to_text():
-    config = pocketsphinx.Decoder.default_config()
-    config.set_string('-hmm', 'path_to_acoustic_model')
-    config.set_string('-lm', 'path_to_language_model')
-    config.set_string('-dict', 'path_to_dictionary')
+    r = sr.Recognizer()
+    r.energy_threshold = 4000  # Adjust the value according to your audio input
+    indian_english_model_path = "voicetotext/cmusphinx-en-in-5.2"
 
-    decoder = pocketsphinx.Decoder(config)
+    # Set paths to Indian English acoustic and language models
+    config = {
+        'hmm': f'{indian_english_model_path}/en_in.cd_cont_5000',
+        'lm': f'{indian_english_model_path}/en-us.lm.bin',
+        'dict': f'{indian_english_model_path}/en_in.dic'
+    }
+    r.recognize_sphinx.set_property('pocketsphinx', config)
 
-    with open('audio_file.wav', 'rb') as audio_file:
-        decoder.start_utt()
-        while True:
-            buf = audio_file.read(1024)
-            if buf:
-                decoder.process_raw(buf, False, False)
-            else:
-                break
-        decoder.end_utt()
+    with sr.Microphone() as source:
+        print("Speak now...")
+        audio = r.listen(source)  # Listen for audio input
 
-    hypothesis = decoder.hyp()
-    if hypothesis is not None:
-        return hypothesis.hypstr
-    else:
-        return ""
+    try:
+        text = r.recognize_sphinx(audio, language='en-IN')
+        return text
+    except sr.UnknownValueError:
+        print("Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Speech Recognition service; {0}".format(e))
 
 # Example usage
 result = voice_to_text()
