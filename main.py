262987b84934ab45b0f21741925dfa8da09b2552
ChristyBinu-4 <christy.binu91@gmail.com>
2023-08-08 12:24:44 +0530

    code rearranging

--- main.py
+++ main.py
@@ -10,13 +10,16 @@ from textTovoice.voice import text_to_speech_speed
 
 import time
 
+
 person_name = main.main()
 #print (person_name)
+
 if person_name == "multiple":
         bot_resp = "Hello Multiple people"
         text_to_speech(bot_resp)
         
 text_to_speech_speed("I am UP....", 100 )
+
 while True :
     recognizedText =  SR.speech()
     if person_name != None and person_name != "multiple":
@@ -24,6 +27,7 @@ while True :
     else:   
         bot_response = chatbot.chat(message=recognizedText)
 
+    #Taking bot reply and related intent from tuple
     bot_reply = bot_response[0]
     reply_intent = bot_response[1] # intent trigger
     
